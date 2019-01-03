af70bb211799dc57dd40e7f1d7c763a83e3b9599
Felipe Lima <felipe.lima@gmail.com>
2019-01-02 20:55:55 -1000

    Add webapp files

---
+++ build.sh
@@ -0,0 +1,1 @@
+cargo build --target wasm32-unknown-unknown && wasm-pack build && wasm-pack pack
--- src/engine/player.rs
+++ src/engine/player.rs
@@ -24,12 +24,12 @@ impl Player {
         Piece { piece_type: PieceType::Pawn, color: color, pos: (6, pawn_pos) },
         Piece { piece_type: PieceType::Pawn, color: color, pos: (7, pawn_pos) },
         Piece { piece_type: PieceType::Rook, color: color, pos: (0, pos) },
-        Piece { piece_type: PieceType::Bishop, color: color, pos: (1, pos) },
-        Piece { piece_type: PieceType::Knight, color: color, pos: (2, pos) },
+        Piece { piece_type: PieceType::Knight, color: color, pos: (1, pos) },
+        Piece { piece_type: PieceType::Bishop, color: color, pos: (2, pos) },
         Piece { piece_type: PieceType::Queen, color: color, pos: (3, pos) },
         Piece { piece_type: PieceType::King, color: color, pos: (4, pos) },
-        Piece { piece_type: PieceType::Knight, color: color, pos: (5, pos) },
-        Piece { piece_type: PieceType::Bishop, color: color, pos: (6, pos) },
+        Piece { piece_type: PieceType::Bishop, color: color, pos: (5, pos) },
+        Piece { piece_type: PieceType::Knight, color: color, pos: (6, pos) },
         Piece { piece_type: PieceType::Rook, color: color, pos: (7, pos) },
       ]
     }
